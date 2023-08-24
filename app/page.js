0f3a72a9cce2118ac9a5674739e36428f5ad53cc
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-24 13:01:44 -0300

    modifying and adding `breakpoints` on `page.js`

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import OrderSummary from './components/OrderSummary'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="xs:w-[31rem] xs:h-min flex h-full w-full flex-col items-center justify-center rounded-3xl p-0 px-5 py-16">
+      <section className="bg-hero xs:bg-top h-52 w-full rounded-t-3xl bg-cover bg-center bg-no-repeat"></section>
+      <OrderSummary />
+    </main>
+  )
 }
