ca0fa9986078024b83f4314d0c52e89a5f403f4f
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-24 13:00:32 -0300

    modifying `talwind.config.js` and `globals.css`

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      fontSize: {
+        body: '1rem',
+      },
+      fontFamily: {
+        'red-hat': ['var(--font-red-hat)'],
+      },
+      backgroundImage: {
+        mobile: ['url(./images/pattern-background-mobile.svg)'],
+        desktop: ['url(./images/pattern-background-desktop.svg)'],
+        hero: ['url(./images/illustration-hero.svg)'],
+      },
+      screens: {
+        xs: '31.25rem',
+      },
     },
   },
   plugins: [],
